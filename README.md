eabc16b1656c4ffb32b488287303dbd0ce797563
Jeff Knox <knoxjeffrey@outlook.com>
2017-10-11 22:40:06 +0100

    calendar and pop up dialog

--- README.md
+++ README.md
@@ -41,9 +41,6 @@ Or install it yourself as:
 //= require active_admin_slickr
 ```
 
-## Generator
-rails g active_admin_slickr:install
-
 ## Development
 
 After checking out the repo, run `bin/setup` to install dependencies. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
--- app/assets/javascripts/active_admin_slickr.js
+++ app/assets/javascripts/active_admin_slickr.js
@@ -1,1 +1,15 @@
-var test = '';
+$(document).ready(function() {
+  // wrapper for filter input fields
+  $('.filter_form_field').wrapAll('<div class="filter_fields" />');
+
+  // wrapper for ui-dialogue
+  $('.batch_action').click(function() {
+    $('.ui-dialog').wrap('<div class="dialog_wrapper" />');
+    $(".ui-dialog-titlebar button").text('');
+
+    $(".ui-dialog").find(".ui-button").click(function () {
+      $('.dialog_wrapper').remove();
+      $('#dialog_confirm').remove();
+    });
+  });
+});
