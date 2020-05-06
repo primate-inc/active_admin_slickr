fcd9dc22c2d0a74f3f848e6f5170e850bb482a62
Bart O <bart@primate.co.uk>
2020-05-06 21:53:02 +0100

    removed test spec

--- spec/active_admin_slickr_test/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
